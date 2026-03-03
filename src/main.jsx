d1ecde487f47042bda522ee5f15d1e890f46fce5
prasanthjj <72882479+prasanthjj@users.noreply.github.com>
2026-03-03 23:16:06 +0530

    Fix import case for Dashboard component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Dashboard from './Dashboard.jsx'
+import Dashboard from './dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode><Dashboard /></StrictMode>
